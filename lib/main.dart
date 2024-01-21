4389f1e03c835fe6bf46e39b6cedd36fc11717bf
RenatoPorfirio <renatoporfiriox@gmail.com>
2024-01-21 19:38:36 -0300

    alter

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'a',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 150, 120, 200)),
         ),
         home: MyHomePage(),
       ),
@@ -37,19 +37,47 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var current = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asString),
-          ElevatedButton(
-            onPressed: () {
-              appState.newWord();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      backgroundColor: Color.fromARGB(255, 200, 150, 150),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(current: current),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.newWord();
+              },
+              child: Text('Próxima Palavra'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.current,
+  });
+
+  final WordPair current;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary);
+    return Card(
+      elevation: 12,
+      color: theme.primaryColor,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(current.asPascalCase, style: style, semanticsLabel: "${current.first} ${current.second}",),
       ),
     );
   }
